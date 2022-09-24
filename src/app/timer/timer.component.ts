fc445b7c41a1d1f6c0fd9f273385af4835cbe1a9
theGeekkit <sonofgondolin16@gmail.com>
2022-09-24 17:29:38 -0500

    9-24-22

---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,17 @@
+import { DirectiveMeta } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.css']
+})
+export class TimerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
