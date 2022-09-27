ab1934739d459a3af9fe64efd9a73f04debe7f3d
theGeekkit <sonofgondolin16@gmail.com>
2022-09-27 18:34:04 -0500

    9-27-22

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,68 +1,40 @@
 import { DirectiveMeta } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.css']
 })
-export class TimerComponent implements OnInit {
+export class TimerComponent implements OnDestroy {
+  counter: number;
+  timerRef;
+  running: boolean = false;
+  startText = 'Start';
 
-  constructor() { }
+  startTimer() {
+    this.running = !this.running;
+    if (this.running) {
+      this.startText = 'Stop';
+      const startTime = Date.now() - (this.counter || 00);
+      this.timerRef = setInterval(() => {
+        this.counter = Date.now() - startTime;
+      });
+    } else {
+      this.startText = 'Resume';
+      clearInterval(this.timerRef);
+    }
+  }
 
-  ngOnInit(): void {
+  clearTimer() {
+    this.running = false;
+    this.startText = 'Start';
+    this.counter = undefined;
+    clearInterval(this.timerRef);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timerRef);
   }
 
 }
-
-const startButton = document.querySelector('[data-action="start"]');
-const stopButton  = document.querySelector('[data-action="stop"]');
-const resetButton = document.querySelector('[data-action="reset"]');
-const minutes     = document.querySelector('.minutes');
-const seconds     = document.querySelector('.seconds');
-let timerTime     = 00;
-let isRunning     = false;
-let interval;
-
-
-            // console.count('hello!');
-
-
-function startTimer() {
-    if (isRunning) return;
-
-    isRunning = true;
-    interval = setInterval(incrementTimer, 1000);
-}
-
-function stopTimer() {
-    if (!isRunning) return;
-
-    isRunning = false;
-    clearInterval(interval);
-}
-
-function resetTimer() {
-    stopTimer();
-
-    timerTime = 0;
-    minutes.innerText = '00';
-    seconds.innerText = '00';
-}
-function incrementTimer() {
-    timerTime++;
-
-    const numOfMinutes = Math.floor(timerTime / 60);
-    const numOfSeconds = timerTime % 60;
-
-    minutes.innerText = pad(numOfMinutes);
-    seconds.innerText = pad(numOfSeconds);
-}
-
-function pad(number) {
-    return (number < 10) ? '0' + number : number;
-}
-
-startButton.addEventListener('click', startTimer);
-stopButton.addEventListener('click', stopTimer);
-resetButton.addEventListener('click', resetTimer);
